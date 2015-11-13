fd4da6ced91e9c85bc04687d9c97e12ff4ee9c84
jlegrone <jacob.legrone@gmail.com>
2015-11-13 14:25:57 -0800

    Lookup or Create Constituents from Email

--- functions.py
+++ functions.py
@@ -1,7 +1,6 @@
-from bsdapi.URL import URL
 from bsdapi.BsdApi import Factory
-import bsdapi
 import json
+import xmltodict
 
 import settings
 
@@ -13,49 +12,72 @@ api = Factory().create(
     securePort='443'
 )
 
+
 def check_credentials(form):
     resp = api.account_checkCredentials(form['email'], form['pw'])
     return resp.body
 
+
 def create_account(form):
 
-    if (form['password1'] != form['password2']):
+    if form['password1'] != form['password2']:
         return 'passwords do not match'
     resp = api.account_createAccount(form['email'], form['password1'], form['firstname'], form['lastname'], form['zip'])
     return resp.body
 
-def create_events(form, *args, **kwargs):
+
+def fetch_constituent(creator_email):
+    # resp = api.doRequest('cons/get_constituents_by_email', api.POST, body={"emails": creator_email})
+
+    # resp = api.cons_getConstituents({'email': creator_email})
+    resp = api.doRequest('/cons/get_constituents_by_email', None, api.POST, body={"emails": creator_email})
+
+    # Parse the xml response to a dict object
+    doc = xmltodict.parse(resp.body)
+
+    if doc['api'] is not None:
+        return doc['api']['cons']['@id']
+    else:
+        params = '<?xml version="1.0" encoding="utf-8"?><api><cons send_password="y"><cons_email><email>' + creator_email + '</email></cons_email></cons></api>'
+        resp = api.doRequest('/cons/set_constituent_data', None, api.POST, body=params)
+        doc = xmltodict.parse(resp.body)
+        return doc['api']['cons']['@id']
+
+
+def create_events(form):
 
     # validations
     # Remove special characters from phone number
     contact_phone = ''.join(e for e in form['contact_phone'] if e.isalnum())
 
+    constituent_id = fetch_constituent(form['cons_email'])
+
     params = {
         'event_type_id': form['event_type_id'],
-        'creator_cons_id': '1', # hardcoded for now
-        'name' : form['name'],
+        'creator_cons_id': constituent_id,
+        'name': form['name'],
         'description': form['description'],
         'venue_name': form['venue_name'],
         'venue_zip': form['venue_zip'],
         'venue_city': form['venue_city'],
         'venue_state_cd': form['venue_state_cd'],
-        'venue_addr1' : form['venue_addr1'],
-        'venue_addr2' : form['venue_addr2'],
-        'venue_country' : form['venue_country'],
-        'venue_directions' : form['venue_directions'],
-        'days' : [{
-            'start_datetime_system': '', # This will be defined below
+        'venue_addr1': form['venue_addr1'],
+        'venue_addr2': form['venue_addr2'],
+        'venue_country': form['venue_country'],
+        'venue_directions': form['venue_directions'],
+        'days': [{
+            'start_datetime_system': '',  # This will be defined below
             'duration': int(form['duration_num']) * int(form['duration_unit']),
-            'capacity' : form['capacity']
+            'capacity': form['capacity']
         }],
-        'local_timezone' : form['start_tz'],
-        'attendee_volunteer_message' : form['attendee_volunteer_message'],
-        'is_searchable' : form['is_searchable'],
-        'public_phone' : form['public_phone'],
-        'contact_phone' : contact_phone,
-        'host_receive_rsvp_emails' : form['host_receive_rsvp_emails'],
-        'rsvp_use_reminder_email' : form['rsvp_use_reminder_email'],
-        'rsvp_reminder_hours' : form['rsvp_email_reminder_hours']
+        'local_timezone': form['start_tz'],
+        'attendee_volunteer_message': form['attendee_volunteer_message'],
+        'is_searchable': form['is_searchable'],
+        'public_phone': form['public_phone'],
+        'contact_phone': contact_phone,
+        'host_receive_rsvp_emails': form['host_receive_rsvp_emails'],
+        'rsvp_use_reminder_email': form['rsvp_use_reminder_email'],
+        'rsvp_reminder_hours': form['rsvp_email_reminder_hours']
     }
 
     if form['start_time[a]'] == 'pm':
@@ -68,9 +90,5 @@ def create_events(form, *args, **kwargs):
     for day in event_dates:
         start_time = day['date'] + ' ' + str(start_hour) + ':' + form['start_time[i]'] + ':00'
         params['days'][0]['start_datetime_system'] = start_time
-        resp = api.doRequest('/event/create_event', None, api.POST, body={"event_api_version": 2, "values": json.dumps(params)})
-
-    # return start_time
-    return resp.body
-
-# called like this:  bsd.create_event(1, 6, "name", "description", "venue_name", "11217", "venue_city", "venue_state_cd")
+        response = api.doRequest('/event/create_event', None, api.POST, body={"event_api_version": 2, "values": json.dumps(params)})
+    return response.body
--- server.py
+++ server.py
@@ -1,119 +1,27 @@
 import flask
-import flask.ext.login as flask_login
-
 from functions import create_events
-from functions import check_credentials
-from functions import create_account
 
 app = flask.Flask(__name__)
-login_manager = flask_login.LoginManager()
-login_manager.init_app(app)
-
-
-
-# Our mock database.
-users = {'foo@bar.tld': {'pw': 'secret'}}
-
-class User(flask_login.UserMixin):
-    pass
-
-
-@login_manager.user_loader
-def user_loader(email):
-    if email not in users:
-        return
-
-    user = User()
-    user.id = email
-    return user
-
-
-@login_manager.request_loader
-def request_loader(request):
-    email = request.form.get('email')
-    if email not in users:
-        return
-
-    user = User()
-    user.id = email
-
-    # DO NOT ever store passwords in plaintext and always compare password
-    # hashes using constant-time comparison!
-    user.is_authenticated = request.form['pw'] == users[email]['pw']
-
-    return user
-
-# User management routes
-
-@app.route('/create-account', methods=['GET', 'POST'])
-def createaccount():
-	if flask.request.method == 'GET':
-		return flask.render_template('login.html')
-
-	response = create_account(flask.request.form)
-	app.logger.debug(response)
-
-	return response
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if flask.request.method == 'GET':
-        return '''
-               <form action='login' method='POST'>
-                <input type='text' name='email' id='email' placeholder='email'></input>
-                <input type='password' name='pw' id='pw' placeholder='password'></input>
-                <input type='submit' name='submit'></input>
-               </form>
-               '''
-
-    response = check_credentials(flask.request.form)
-    app.logger.debug(response)
-
-    return response
-
-    email = flask.request.form['email']
-    if flask.request.form['pw'] == users[email]['pw']:
-        user = User()
-        user.id = email
-        flask_login.login_user(user)
-        return flask.redirect(flask.url_for('protected'))
-
-    return 'Bad login'
-
-@app.route('/logout')
-def logout():
-    flask_login.logout_user()
-    return 'Logged out'
-
-@app.route('/protected')
-@flask_login.login_required
-def protected():
-    return 'Logged in as: ' + flask_login.current_user.id
-
-@login_manager.unauthorized_handler
-def unauthorized_handler():
-    return 'Unauthorized'
-
-
-
 
 
 # App Routes
 @app.route('/')
 def index():
-	return flask.render_template('create_event.html', debug=app.debug)
+    return flask.render_template('create_event.html', debug=app.debug)
+
 
 @app.route('/create', methods=['POST'])
 def create():
-	items = "\n"
-	for key in flask.request.form:
-		items += key + ' - ' + str(flask.request.form[key]) + '\n'
-	app.logger.debug(items)
+    # Log all the data sent from the form (for debugging purposes only)
+    items = "\n"
+    for key in flask.request.form:
+        items += key + ' - ' + str(flask.request.form[key]) + '\n'
+    app.logger.debug(items)
 
-	response = create_events(flask.request.form)
+    response = create_events(flask.request.form)
+    app.logger.debug(response)
+    return response
 
-	app.logger.debug(response)
-	return response
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 5000, debug=True)
